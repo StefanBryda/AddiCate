4ef90820abce1ccf82f76c2f7f9cc360ed4d4af4
StefanBryda <stefan.bryda@student.nhlstenden.com>
2025-06-18 17:49:46 +0200

    Set up the day system

--- addicate/scripts/door_outside.gd
+++ addicate/scripts/door_outside.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://scenes/Outside.tscn")
+		get_tree().change_scene_to_file("res://scenes/day1/Outside.tscn")
--- addicate/scripts/flower.gd
+++ addicate/scripts/flower.gd
@@ -3,10 +3,10 @@ extends Area2D
 @export var interactable := true
 
 func _ready():
-    if not interactable:
-        set_process_input(false)
-        modulate = Color(1, 1, 1, 0.5)  # semi-transparent
+	if not interactable:
+		set_process_input(false)
+		modulate = Color(1, 1, 1, 0.5)  # semi-transparent
 
 func _input_event(viewport, event, shape_idx):
-    if interactable and event is InputEventMouseButton and event.pressed:
-        queue_free()
+	if interactable and event is InputEventMouseButton and event.pressed:
+		queue_free()
