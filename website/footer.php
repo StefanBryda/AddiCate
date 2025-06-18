0aaecd9c6dc584b6b8acd860ba5d18be2bea976b
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-06-18 02:20:35 +0200

    Added story sharing idea and social links

--- website/footer.php
+++ website/footer.php
@@ -1,3 +1,30 @@
 <footer>
-  &copy; 2025 - AddiCate, Inc. All rights reserved. Address Address
+
+      <!-- Left Footer -->
+      <div class="footer-left">
+
+        <div class="social-links">
+          <a href="#">Instagram</a>
+          <a href="#">Facebook</a>
+          <a href="#">LinkedIn</a>
+        </div>
+
+        <p>&copy; 2025 - AddiCate, Inc. All rights reserved. Address Address</p>
+        <p>
+          <a href="#">Privacy Policy</a> | <a href="#">Terms & Conditions</a>
+        </p>
+
+      </div>
+
+      <!-- Right Footer -->
+      <h3>Send us your story and be heard. <b>Your Story Matters</b></h3>
+      <form class="story-form">
+        <input type="text" placeholder="Tell us your story">
+        <button type="submit">Send</button>
+      </form>
+
+    </div>
+
+  </div>
+
 </footer>
