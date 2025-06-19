a836a2a62bfdae3a42b785fce8f4d9e9f969d59d
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-06-19 17:08:41 +0200

    added video links

--- website/exp-videos.php
+++ website/exp-videos.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Videos</title>
     <link rel="stylesheet" href="./styleSheets/mainStyle.css" type="text/css">
+    <link rel="stylesheet" href="./styleSheets/exp-videopage.css" type="text/css">
 </head>
 
 <body>
@@ -18,19 +19,37 @@
         <section class="video-grid">
             <div class="video-container">
                 <video controls>
-                    <source src="your-video.mp4" type="video/mp4">
+                    <source src="./videos/What is Addiction.mov" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
             </div>
 
             <div class="video-info">
-                <h2>Story Behind the Struggle</h2>
+                <h2>What is Addiction</h2>
                 <p>
                     This video walks you through the journey of someone living with addiction.
                     From daily struggles to moments of recovery, it provides emotional insight
                     and educational value. Watch carefully and reflect on how support can change lives.
                 </p>
             </div>
+        </section>
+
+        <section class="video-grid">
+
+            <div class="video-info">
+                <h2>Stages of Addiction</h2>
+                <p>
+                    This video walks you through the stages of Addiction.
+                </p>
+            </div>
+
+            <div class="video-container">
+                <video controls>
+                    <source src="./videos/Stages of Addiction.mov" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+
         </section>
     </main>
 
