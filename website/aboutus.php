0d5d368ad04e686fb992e4f2403fa1761d3538cb
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-06-19 19:12:51 +0200

    added css link + adjusted the box's, text alignment, and image border

--- website/aboutus.php
+++ website/aboutus.php
@@ -1,47 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About us</title>
     <link rel="stylesheet" href="./styleSheets/mainStyle.css" type="text/css">
+    <link rel="stylesheet" href="./styleSheets/aboutuspage.css" type="text/css">
 </head>
 
 <body>
-<?php include 'header.php'; ?>
+    <?php include 'header.php'; ?>
 
-<main>
-    <h1 class="aboutus-title">About Us</h1> 
-    <div class="aboutus-main-container">
+    <main>
+
+        <h1 class="aboutus-title">About Us</h1>
+
+        <div class="aboutus-main-container">
+
             <div class="aboutus-box1">
-                <h3>Reasons for the Project</h3>
-                <p>The project is developed to raise awareness that is focused on addiction, 
-                which not only affects individuals who suffer from any form of addiction, but also others around them, 
-                for example their families, friends and broader communities. 
-                Addressing the needs of both people who are struggling with addiction and those who support them is the aim of the project, 
-                by providing support and resources.</p> 
+                <h3>Our Journey</h3>
+                <hr>
+                <p>The project is developed to raise awareness that is focused on addiction,
+                    which not only affects individuals who suffer from any form of addiction, but also others around them,
+                    for example their families, friends and broader communities.
+                    Addressing the needs of both people who are struggling with addiction and those who support them is the aim of the project,
+                    by providing support and resources.
+                </p>
             </div>
-            <div class="aboutus-box2"></div>
-        
+
+
             <div class="aboutus-box3">
                 <img id="group-picutre" src="./images/group-picture.png" alt="group-picutre">
             </div>
+
             <div class="aboutus-box4">
                 <h3>Project Members</h3>
-                    <ul>
-                        <li>Stefania Panainte – <em>Project Manager</em></li>
-                        <li>Eliza Horváth</li>
-                        <li>Airaa Srinivasan</li>
-                        <li>Stefan Bryda</li>
-                        <li>Miriam Cerulíková</li>
-                        <li>Aleksandra Sznajder</li>
-                        <li>Fabiana Brando</li>
-                    </ul>
+                <hr>
+                <ul class="member-list">
+                    <li>Stefania Panainte - <strong>Project Manager</strong></li>
+                    <li>Eliza Horváth</li>
+                    <li>Airaa Srinivasan</li>
+                    <li>Stefan Bryda</li>
+                    <li>Miriam Cerulíková</li>
+                    <li>Aleksandra Sznajder</li>
+                    <li>Fabiana Brando</li>
+                </ul>
             </div>
-    </div>
-</main>
 
-<?php include 'footer.php'; ?>
-    
+        </div>
+
+    </main>
+
+    <?php include 'footer.php'; ?>
+
 </body>
+
 </html>
