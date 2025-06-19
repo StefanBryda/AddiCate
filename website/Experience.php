21b392fc9d4cc18531ba551f58a51717298e5e70
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-06-19 17:31:46 +0200

    Added images and center aligned the story and video box

--- website/Experience.php
+++ website/Experience.php
@@ -7,6 +7,7 @@
     <title>Experience
     </title>
     <link rel="stylesheet" href="./styleSheets/mainStyle.css" type="text/css">
+    <link rel="stylesheet" href="./styleSheets/experiencepage.css" type="text/css">
 </head>
 
 <body>
@@ -18,25 +19,29 @@
 
         <div class="experience-wrapper">
             <!-- Left animation placeholder -->
-            <div class="experience-anim anim-left">
-                <!-- Add your animation or image here -->
-            </div>
+            <!-- <div class="experience-anim anim-left"> -->
+            <!-- Add your animation or image here -->
+            <!-- </div> -->
 
             <!-- Central experience options -->
             <section class="experience-grid">
+
                 <a href="exp-story.php" class="exp-box">
+                    <img src="./images/writing-story.jpg" alt="Story 2">
                     <p>Discover others' experiences and journeys</p>
                 </a>
 
                 <a href="exp-videos.php" class="exp-box">
+                    <img src="./images/play-button-video.jpg" alt="Story 2">
                     <p>Watch videos to understand addiction better</p>
                 </a>
+
             </section>
 
             <!-- Right animation placeholder -->
-            <div class="experience-anim anim-right">
-                <!-- animation or image here -->
-            </div>
+            <!-- <div class="experience-anim anim-right"> -->
+            <!-- animation or image here -->
+            <!-- </div> -->
         </div>
     </main>
 
