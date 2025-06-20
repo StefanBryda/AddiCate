afe9623fec3f7a4c239cca559d3913f85753b397
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-06-20 20:27:48 +0200

    added notification message div and javascript for notification

--- website/game_page.php
+++ website/game_page.php
@@ -2,16 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./styleSheets/mainStyle.css" rel="stylesheet" type="text/css">
-    <link href="./styleSheets/gamepage.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Game Page</title>
+    <link href="./styleSheets/mainStyle.css" rel="stylesheet" type="text/css" />
+    <link href="./styleSheets/gamepage.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 
     <?php include 'header.php'; ?>
+
+    <div id="notification-message" class="hidden">
+        
+        <p id="notification-text"></p>
+   
+    </div>
 
     <div class="game-container">
 
@@ -19,30 +25,30 @@
             <h1>Game</h1>
         </div>
 
-
         <div class="game-split-container">
 
-            <!-- Left Side - Game Embed -->
+            <!-- Game -->
             <div class="game-left-container">
 
                 <div class="game-embed">
 
                     <iframe id="godot-game" src="../addicate/exports/web/demo3/AddicateGameDemo3.html" frameborder="0" allowfullscreen></iframe>
-
+                
                 </div>
 
             </div>
 
-            <!-- Right Side - Description and Review -->
+            <!-- Right Side -->
             <div class="game-right-container">
 
                 <div class="game-description">
 
                     <h2>Game Story</h2>
 
-                    <p>By participating in this game, you will be able to experience the life of Greg. You will see and learn, how easily a person can get addicted to anything.<br>
-                    CONTENT WARNING: Game contains motives of addiction<br>
-                    (This is a demo version 3)
+                    <p>
+                        Experience the life of Greg. Learn how easy it is to get addicted.
+                        <br>⚠️ Content Warning: Game contains addiction-related content.
+                        <br><i>This is a demo version 3.</i>
                     </p>
 
                 </div>
@@ -50,23 +56,26 @@
                 <div class="game-review">
 
                     <h2>Leave a Review</h2>
-                    <form id="review-form" action="mailto:projectaddicate25@gmail.com" method="post" enctype="text/plain">
+
+                    <form id="review-form" action="submit_review.php" method="post">
 
                         <textarea name="review" placeholder="Type your anonymous review here..." required></textarea>
 
                         <button type="submit">Submit Review</button>
 
                     </form>
-
                 </div>
 
             </div>
 
         </div>
+
     </div>
 
     <?php include 'footer.php'; ?>
 
+    <script src="./scripts/notification.js" defer></script>
+    
 </body>
 
 </html>
