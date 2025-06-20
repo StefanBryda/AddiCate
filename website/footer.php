889e315de0d02dd778e6919882b81de184a94b35
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2025-06-20 22:04:55 +0200

    took out the commented work

--- website/footer.php
+++ website/footer.php
@@ -16,13 +16,6 @@
 
       </div>
 
-      <!-- Right Footer -->
-      <!-- <h3>Send us your story and be heard. <b>Your Story Matters</b></h3>
-      <form class="story-form">
-        <input type="text" placeholder="Tell us your story">
-        <button type="submit">Send</button>
-      </form> -->
-
     </div>
 
   </div>
